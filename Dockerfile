7fdff2724b9a9af707da91da697ed1c8549e99e1
pr0totyp <prototyp@d00m.org>
2019-09-18 01:04:23 +0200

    Replace env vars via envsubst

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM postgres:alpine
 MAINTAINER coderat-collective
 
+# Install envsubst
+RUN apk add --no-cache curl
+RUN curl -L https://github.com/a8m/envsubst/releases/download/v1.1.0/envsubst-`uname -s`-`uname -m` -o /usr/local/bin/envsubst
+RUN chmod +x /usr/local/bin/envsubst
+
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 COPY docker-cmd.sh /docker-cmd.sh
 COPY data/scripts /scripts
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Import your cron file
-/usr/bin/crontab /mnt/config/crontab.txt
+
+envsubst < /mnt/config/crontab.txt > /root/crontab.txt
+/usr/bin/crontab /root/crontab.txt
 
 # Convert Docker Environment vars to vars needed by pg_backup_rotated.sh
-/scripts/config_gen.sh
+
+envsubst < /mnt/config/pg_backup.config > /root/pg_backup.config
 
 # Run the command instruction
 exec "$@"
